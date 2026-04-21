d7ee3f38e309175dbbd5d1867d14d5cfd69b4408
wgflather <emecevgenij@gmail.com>
2026-04-21 16:18:41 +0300

    Add controllers console logging and ERROR/WARN file logging

--- src/main/java/com/flather/weatherstation/controller/WeatherController.java
+++ src/main/java/com/flather/weatherstation/controller/WeatherController.java
@@ -1,10 +1,12 @@
 package com.flather.weatherstation.controller;
 
+import com.flather.weatherstation.WeatherStationApplication;
 import com.flather.weatherstation.entity.WeatherRecordCreatedDto;
 import com.flather.weatherstation.entity.WeatherRecordResponseDto;
 import com.flather.weatherstation.service.WeatherService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -13,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
+@Slf4j
 public class WeatherController {
 
     static final String BASE_PATH = "/api/weather";
@@ -20,16 +23,19 @@ public class WeatherController {
 
     private final WeatherService service;
 
+
     @PostMapping(BASE_PATH)
     public ResponseEntity<Void> saveWeatherRecord(@Valid @RequestBody WeatherRecordCreatedDto dto){
         WeatherRecordResponseDto savedRecord = service.saveWeatherRecord(dto);
-        System.out.println(savedRecord.toString());
+        log.info("Saved a new weather record: {}", savedRecord);
+
         return ResponseEntity.noContent().build();
 
     }
 
     @GetMapping(LATEST_WEATHER_PATH)
     public ResponseEntity<WeatherRecordResponseDto> getLatestWeatherRecord(){
+
         return ResponseEntity.ok(service.getLatestWeatherRecord().orElse(null));
     }
 
--- src/main/java/com/flather/weatherstation/controller/WeatherDashboardController.java
+++ src/main/java/com/flather/weatherstation/controller/WeatherDashboardController.java
@@ -4,14 +4,17 @@ import com.flather.weatherstation.entity.WeatherRecordResponseDto;
 import com.flather.weatherstation.mapper.WeatherRecordMapper;
 import com.flather.weatherstation.service.WeatherService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import java.time.format.DateTimeFormatter;
+import java.util.Optional;
 
 @Controller
 @RequiredArgsConstructor
+@Slf4j
 public class WeatherDashboardController {
     static final String BASE_PATH = "/weather";
 
@@ -20,9 +23,9 @@ public class WeatherDashboardController {
 
     @GetMapping(BASE_PATH)
     public String getLatestWeather(Model model){
-
-         model.addAttribute("weather", service.getLatestWeatherRecord().orElse(null));
-        System.out.println(service.getLatestWeatherRecord());
+        Optional<WeatherRecordResponseDto> dto = service.getLatestWeatherRecord();
+        model.addAttribute("weather", dto.orElse(null));
+        log.info("Retrieved Weather Record: {}",dto);
 
         return "weather_dashboard";
     }
