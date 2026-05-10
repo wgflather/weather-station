66da16a00ebc4395c01820f5b77466f12ee11879
Your Name <you@example.com>
2026-05-10 10:44:50 +0300

    Add api error response dto

---
+++ src/main/java/com/flather/weatherstation/dto/error/ApiErrorResponse.java
@@ -0,0 +1,12 @@
+package com.flather.weatherstation.dto.error;
+
+import java.net.URI;
+import java.time.Instant;
+
+public record ApiErrorResponse(
+        Instant timestamp,
+        int status,
+        String error,
+        String message,
+        URI path) {
+}
